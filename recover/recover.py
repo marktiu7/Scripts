311c586736d27713039aeed4bc5f4705361fc2d4
Mark Tiu <marktiu71@Outlook.com>
2017-09-10 20:18:29 +0800

    create file

---
+++ recover/recover.py
@@ -0,0 +1,87 @@
+#!/usr/bin/python
+
+import time,sys,threading,os
+import subprocess as sub
+
+user='dbuser'
+pwd='123456'
+path="/root/python/test/bak"
+t=1
+dbnamelist=[]
+dblist=[]
+
+
+class Mainfunc():
+     def __init__(self,cmd):
+       self.cmd=cmd
+     
+     def subcommand(self):
+       try:
+         ret = sub.Popen(self.cmd,shell=True,stdout=sub.PIPE,stderr=sub.PIPE)
+         out,err = ret.stdout.read(),ret.stderr.read()
+         result = out if out else err
+      #   print(result.decode('gb2312'))
+       except Exception as f:
+         print(f)
+
+
+cmd = "gunzip %s/*.sql.gz " % path
+un = Mainfunc(cmd)
+print("All of things will be unziped...")
+un.subcommand()
+print("Fininshed!!!")
+sourcelist=os.listdir(path)
+    
+def get_dbname():
+    for i in sourcelist:
+        dbname,*m = i.split('.')
+        dbnamelist.append(dbname)
+
+
+    for db in dbnamelist:
+        cmd = "cat list.txt| grep %s | awk '{print $8 }'" % db
+        child = sub.Popen(cmd,shell=True,stdout=sub.PIPE)
+        res = child.stdout.read()
+        dblist.append(res.decode('gb2312').replace('\n',''))
+
+
+def sqlcommand():
+    for dbname,db,source in zip(dbnamelist,dblist,sourcelist):
+        sqlconnect = "mysql -u%s -h%s -p%s" % (user,db,pwd)
+        sqlcmd = [ sqlconnect + " -e 'drop  databases %s if exists;'" % dbname,
+                  sqlconnect + "  < %s/%s " % (path,source)]
+                     
+    for s in sqlcmd:
+        recov = Mainfunc(s)
+        recov.subcommand()
+
+    global t
+    t = 0.01    
+    
+def view_bar():
+   for i in range(0,101):
+      time.sleep(t)
+      rate = i /100
+      rate_num= int(rate*100)
+      r = '\r[%s%s]%d%%' %("="*i," "*(100-i),rate_num)
+      sys.stdout.write(r)
+      sys.stdout.flush()
+   print()
+      
+def mupt():
+    thlist=[]
+    s = threading.Thread(target=sqlcommand)
+    v = threading.Thread(target=view_bar)
+    thlist.append(s)
+    thlist.append(v)
+
+    for m in thlist:
+        m.start()
+
+    for m in thlist:
+        m.join()
+
+if __name__=="__main__":
+   get_dbname()
+   
+   mupt()
